15ddd1d846354b13570c5038b1a2b6567dd9a7fe
Kostya Likhachev <arrow.koss@gmail.com>
2019-07-10 09:22:02 +0300

    - initial project

---
+++ Rx-Simple/Rx-Simple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rx-Simple
+//
+//  Created by mac on 7/10/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
